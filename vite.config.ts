9ce7765ec74a1e5455e866fe2d6b27f35673f95c
tomoemon <bach48+github_tomoemon@gmail.com>
2023-07-22 23:20:30 +0900

    setup ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     include: ["**/*.test.ts"],
   },
-  plugins: [react()],
+  plugins: [],
   build: {
     sourcemap: true,
     lib: {
