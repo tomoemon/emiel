510cf575771a6363e11a7b5c8ebed3bde64b4277
tomoemon <bach48+github_tomoemon@gmail.com>
2023-08-06 18:00:42 +0900

    fix packaging

--- vite.config.ts
+++ vite.config.ts
@@ -11,23 +11,15 @@ export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
-      // 複数のエントリーポイントのディクショナリや配列にもできます
       entry: resolve(__dirname, "src/index.ts"),
       name: "emiel",
-      // 適切な拡張子が追加されます
-      fileName: "emiel",
+      fileName: "index",
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      // ライブラリにバンドルされるべきではない依存関係を
-      // 外部化するようにします
-      external: ["react"],
-      output: {
-        // 外部化された依存関係のために UMD のビルドで使用する
-        // グローバル変数を提供します
-        globals: {
-          vue: "Vue",
-        },
-      },
+      external: [],
+      output: {},
+      plugins: [],
     },
   },
 });
