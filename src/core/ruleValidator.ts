845d6b051d1ba825d6ff773d5f75dbef6457c945
tomoemon <bach48+github_tomoemon@gmail.com>
2024-08-19 23:52:39 +0900

    add comment

--- src/core/ruleValidator.ts
+++ src/core/ruleValidator.ts
@@ -6,28 +6,35 @@ import { RuleStroke } from "./ruleStroke";
  * validation 時にのみ使う関数はメソッドとしてではなく、ここの中に記述する
  */
 
+/**
+ * Rule の中に重複するエントリがないか検証する
+ * 同じ Output でかつ不要な修飾キーのみが異なる Input Stroke を持つエントリがある場合にエラーを投げる
+ * 完全に同じ Output, Input を持つエントリは許容する（同じ RuleEntry があっても矛盾や不整合は生じないため）
+ * @param rule 
+ */
 export function validateRule(rule: Rule): void {
   const outputHashMap = new Map<string, RuleEntry[]>();
   rule.entries.forEach((entry) => {
     if (outputHashMap.has(entry.output)) {
-      const entries = outputHashMap.get(entry.output) as RuleEntry[];
-      entries.forEach((v) => {
-        for (let i = 0; i < v.input.length && i < entry.input.length; i++) {
-          const result = compareStroke(v.input[i], entry.input[i]);
-          if (!result.key || !result.requiredModifier) {
-            // 必要なキーが異なる場合はそもそも StrokeNode として区別可能なので問題ない
+      // 同じ Output を持つ RuleEntry が存在する場合、Input が同じかどうかを確認する
+      const otherEntries = outputHashMap.get(entry.output) as RuleEntry[];
+      otherEntries.forEach((otherEntry) => {
+        for (let i = 0; i < otherEntry.input.length && i < entry.input.length; i++) {
+          const result = compareStroke(otherEntry.input[i], entry.input[i]);
+          if (!result.keyEquals || !result.requiredModifierEquals) {
+            // 必要なキーや修飾キーが異なる場合はそもそも StrokeNode として区別可能なので問題ない
             break;
           }
-          if (!result.unnecessaryModifiers) {
+          if (!result.unnecessaryModifiersEquals) {
             throw new Error(
               `Rule ${rule.name} has duplicated input entry: ${prettyPrint(
                 entry
-              )} and ${prettyPrint(v)}`
+              )} and ${prettyPrint(otherEntry)}`
             );
           }
         }
       });
-      entries.push(entry);
+      otherEntries.push(entry);
     } else {
       outputHashMap.set(entry.output, [entry]);
     }
@@ -35,13 +42,13 @@ export function validateRule(rule: Rule): void {
 }
 
 /**
- * keys と requiredModifiers は一致しているが unnecesaryModifiers が一致していないときに true を返す
+ * Stroke 内に含まれる各要素について一致するかどうかを返す
  */
 function compareStroke(
   thisStroke: RuleStroke,
   other: RuleStroke
-): { key: boolean; requiredModifier: boolean; unnecessaryModifiers: boolean } {
-  const keysResult = thisStroke.key === other.key;
+): { keyEquals: boolean; requiredModifierEquals: boolean; unnecessaryModifiersEquals: boolean } {
+  const keyResult = thisStroke.key === other.key;
 
   const requiredModifierResult = thisStroke.requiredModifier.equals(
     other.requiredModifier
@@ -52,8 +59,8 @@ function compareStroke(
     thisMods.length === otherMods.length &&
     thisMods.every((v) => otherMods.some((v2) => v === v2));
   return {
-    key: keysResult,
-    requiredModifier: requiredModifierResult,
-    unnecessaryModifiers: unnecesaryModifiersResult,
+    keyEquals: keyResult,
+    requiredModifierEquals: requiredModifierResult,
+    unnecessaryModifiersEquals: unnecesaryModifiersResult,
   };
 }
